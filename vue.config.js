77a6f0a0e6d6b70247a796b9d0c3daaa666e8db8
shama <shama.dmitry@gmail.com>
2021-08-05 13:50:53 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://192.168.114.153:3000'
+        // target: 'http://192.168.114.153:3000'
+        target: 'https://pure-sea-27302.herokuapp.com/'
       }
     },
   },
