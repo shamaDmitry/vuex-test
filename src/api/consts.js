cf495cb77fb63c04fcf9962313a4d1e6a4bc6d81
shama <shama.dmitry@gmail.com>
2021-08-09 17:27:19 +0300

    add new api key

--- src/api/consts.js
+++ src/api/consts.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory';
 export const API_EXTERNAL = 'https://openexchangerates.org/api';
-export const APP_ID = '60ddd398d8f04ea080326647b65b1dac';
+export const APP_ID = 'cbed82a8452c4072ae6de6d947664a70';
 export const API = 'https://pure-sea-27302.herokuapp.com';
